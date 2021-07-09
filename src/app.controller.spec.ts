4ccdba4900833382a5176ad9c3b7ca8b194bedd3
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2021-07-09 15:12:44 -0300

    test: Add tests AppController's new endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,8 +16,33 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should be empty', () => {
+      expect(appController.getHello()).toStrictEqual({});
+    });
+  });
+
+  describe('root/:name', () => {
+    it('should return searched term', () => {
+      expect(appController.getNames('lorem')).toStrictEqual(
+        expect.objectContaining({ query: 'lorem' }),
+      );
+    });
+
+    it('should return names that were found', () => {
+      expect(appController.getNames('bailes')).toStrictEqual(
+        expect.objectContaining({
+          results: expect.arrayContaining([
+            {
+              name: 'ISABEL',
+              count: 239654,
+            },
+            {
+              name: 'SIBELA',
+              count: 36,
+            },
+          ]),
+        }),
+      );
     });
   });
 });
