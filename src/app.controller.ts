ab14be44c96f7b8b9bf120a525587674b34c8eef
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2021-07-09 12:37:17 -0300

    feat: Add route to display search results

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FindNamesService } from './findnames.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly findNamesService: FindNamesService,
+  ) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Get(':name')
+  getNames(@Param('name') name: string) {
+    const possibleNames = this.appService.getPossibleNames(name);
+    return this.findNamesService.searchNames(possibleNames);
   }
 }
